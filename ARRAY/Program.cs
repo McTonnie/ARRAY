2535d4c355566ed3d2d79461b04da3ed3d35a229
Oliver Tobias Jensen <Olivertobias@live.dk>
2021-01-27 14:37:39 +0100

    Add project files.

---
+++ ARRAY/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARRAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[9];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
