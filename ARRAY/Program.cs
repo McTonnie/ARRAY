df98251523475c85086234137aa80b63d2ff97a0
Oliver Tobias Jensen <Olivertobias@live.dk>
2021-01-27 14:48:22 +0100

    Der var flere underopgaver

--- ARRAY/Program.cs
+++ ARRAY/Program.cs
@@ -16,6 +16,18 @@ namespace ARRAY
             {
                 numbers[i] = i + 1;
             }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i + 1 == 5)
+                {
+                    //vidste ikke lige om det det dobelte af plads nr 5 (index 4) eller om den fra index 3 så jeg valgte den med index 3
+
+                    numbers[i] = numbers[i - 1] * 2;
+
+                    //eller ville det se sådan her ud kun en af dem skal bruges
+                    numbers[i] = numbers[i] * 2;
+                }
+            }
             foreach (int number in numbers)
             {
                 Console.WriteLine(number);
